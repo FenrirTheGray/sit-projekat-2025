ac066b5795011d61664a0ee0bf53996e63d30c7e
DusanKrstic21000 <dusank281@gmail.com>
2026-01-31 14:13:31 +0100

    changed services

--- app/ordering/src/main/java/sitprojekat/service/UserService.java
+++ app/ordering/src/main/java/sitprojekat/service/UserService.java
@@ -2,6 +2,10 @@ package sitprojekat.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+
 import sitprojekat.dto.LoginRequestDTO;
 import sitprojekat.dto.LoginResponseDTO;
 
@@ -19,14 +23,26 @@ public class UserService {
     }
 
     public boolean login(String email, String password){
+    	
+    	try {
         LoginRequestDTO loginRequest = new LoginRequestDTO(email, password);
         LoginResponseDTO responseDTO = httpService.post(httpService.AUTH_BASE_URL + "/login", loginRequest, LoginResponseDTO.class, false);
 
-        if(responseDTO.token() == null || responseDTO.token().isEmpty()) return false;
+        if(responseDTO.token() == null || responseDTO.token().isEmpty()) {
+        	
+        	Notification notification = Notification.show("uneti podaci se ne podudaraju sa nalogom", 3000, Notification.Position.BOTTOM_CENTER); // sta pise , koliko traje, pozicija
+    	    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);// koje je boje
+        	
+        	
+        	return false;
+        }
 
         userStoreService.setToken(responseDTO.token());
 
         return true;
+    }catch (Exception e) {
+		return false;
+	}
     }
 
     public void logout(){
