9e28c1a40747b8617a1806384f90c16a4f3a4a5d
Joxiaren <2000jox@gmail.com>
2026-02-05 21:35:47 +0100

    Initial files

--- app/cms/src/main/java/rs/ac/singidunum/cms/view/FrequencyProductsView.java
+++ app/cms/src/main/java/rs/ac/singidunum/cms/view/FrequencyProductsView.java
@@ -4,25 +4,23 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import rs.ac.singidunum.cms.presenter.FrequencyProductsPresenter;
 
 @Route(value = "statistics/frequency-products", layout = MasterHeaderNavLayout.class)
 @CssImport("./style/style-views.css")
 public class FrequencyProductsView extends VerticalLayout {
-    // atributi
-    // SellersService - servis za promet
 
-    // konstruktor
-    public FrequencyProductsView() {
-        // naslov
+    private FrequencyProductsPresenter presenter;
+
+    public FrequencyProductsView(FrequencyProductsPresenter presenter) {
+
+        this.presenter = presenter;
+        this.presenter.setView(this);
+
         H1 naslov = new H1("Učestalost");
         naslov.addClassName("page-title");
         add(naslov);
 
-        // inicijalne metode
-        // createSearchBarAndAddButton();
-        // createArticlesContainer();
-
-        // TODO: dodavanje liste proizvoda
 
 
         setSizeFull();
